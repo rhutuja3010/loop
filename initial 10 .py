3cb38730bcb3dd73432c116eb2f56f4305f2e08c
rhutuja3010 <91016307+rhutuja3010@users.noreply.github.com>
2022-05-08 11:05:16 +0530

    Add files via upload

---
+++ initial 10 .py
@@ -0,0 +1,61 @@
+# i=10
+# while i<20:
+#     a=i-9
+#     print(a)
+#     i+=1
+
+# i=1
+# while i<=100:
+#     if i%2==0:
+#         print(-i)
+#     else:
+#         print(i)
+#     i+=1
+
+
+# string=sentence.split( ) 
+# print(string)
+# sentence = "NavGurukul is an alternative to higher education reducing the barriers of current formal education system"
+# a=[]
+# b=" "
+# for i in sentence:
+#     if i==" ":
+#         a.append(b)
+#         b=" "
+#     else:
+#         b+=i
+# if b:
+#     a.append(b)
+# print(a)
+
+k=1
+i=1
+while i<=5:
+    b=1
+    while b<=i:
+        print(" ",end=" ")   
+        b=b+1
+    j=1
+    while j<=k:
+        print("*  ",end=" ")
+        j=j+1
+    k=k+1
+    print()   
+    i=i+1
+k=4
+i=1
+while i<=4:
+    b=1
+    while b<=i:
+        print(" ",end="  ")   
+        b=b+1
+    j=1
+    while j<=k:
+        print("* ",end=" ")      
+        j=j+1
+    k=k-1
+    print() 
+    i=i+1                            
+
+
+
